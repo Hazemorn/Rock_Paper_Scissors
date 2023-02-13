7261f92781dc294a8c9e085093bdc3b50b793bee
Hazemorn <nikitmiheenko@gmail.com>
2023-02-13 20:31:00 +0300

    first commit

---
+++ target/debug/deps/rock_paper_scissors-68f836c9fd09a798.d
@@ -0,0 +1,5 @@
+/home/User/Code/Rust/rock_paper_scissors/target/debug/deps/rock_paper_scissors-68f836c9fd09a798.rmeta: src/main.rs
+
+/home/User/Code/Rust/rock_paper_scissors/target/debug/deps/rock_paper_scissors-68f836c9fd09a798.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rock_paper_scissors-c4d321fe6f4212bb.d
@@ -0,0 +1,5 @@
+/home/User/Code/Rust/rock_paper_scissors/target/debug/deps/rock_paper_scissors-c4d321fe6f4212bb.rmeta: src/main.rs
+
+/home/User/Code/Rust/rock_paper_scissors/target/debug/deps/rock_paper_scissors-c4d321fe6f4212bb.d: src/main.rs
+
+src/main.rs:
